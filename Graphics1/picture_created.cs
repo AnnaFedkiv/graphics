7c0f9bce90c411468bc576122fb16530564dbd0a
AnnaFedkiv <AnnaFedkiv@gmail.com>
2020-03-29 17:36:41 +0200

    Read - draw coordinates

--- Graphics1/picture_created.cs
+++ Graphics1/picture_created.cs
@@ -7,23 +7,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Graphics1
 {
     public partial class picture_created : Form
     {
+        int a, b, c, d;
 
         public picture_created()
         {
             InitializeComponent();
         }
 
-        private void picture_created_Paint(object sender, PaintEventArgs e)
+        public void read()
         {
             System.Drawing.Graphics graphicsobj;
             graphicsobj = this.CreateGraphics();
-            Pen pen_black = new Pen(System.Drawing.Color.Black, 5);
-            graphicsobj.DrawLine(pen_black, 20, 20, 200, 10);
+            var localPath = System.IO.Path.GetDirectoryName(Application.ExecutablePath) + "\\data.txt";
+
+            if (File.Exists(localPath))
+            {
+                // Read a text file line by line.  
+                string[] lines = File.ReadAllLines(localPath);
+                int prevX = 0;
+                int prevY = 0;
+
+                foreach (string line in lines)
+                {
+                    string[] words = line.Split(' ');
+                    if (words[0] != "G1")
+                        continue;
+
+                    string[] lineX = words[1].Split('X');
+                    double x = Convert.ToDouble(lineX[1]);
+
+
+                    string[] lineY = words[2].Split('Y');
+                    double y = Convert.ToDouble(lineY[1]);
+
+                    Pen pen_black = new Pen(System.Drawing.Color.Black, 5);
+
+                    int ix = Convert.ToInt32(x);
+                    int iy = Convert.ToInt32(y);
+
+                    graphicsobj.DrawLine(pen_black, ix, iy, prevX, prevY);
+
+                    prevX = ix;
+                    prevY = iy;
+
+                }
+            }
+        }
+
+        private void picture_created_Paint(object sender, PaintEventArgs e)
+        {
+            read();
         }
     }
 }
